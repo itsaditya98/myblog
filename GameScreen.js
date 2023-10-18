6f35a3c4b1fde04c30b01fbf07436b967fb5b889
Aditya Vinayak <60056272+itsaditya98@users.noreply.github.com>
2023-10-18 17:31:56 +0530

    Delete GameScreen.js

--- GameScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
-
-const GameScreen = () => {
-  return <WebView source={{ uri: 'C:\Users\adity\OneDrive\Documents\GAME2\gamep.html' }} />;
-};
-
-export default GameScreen;
